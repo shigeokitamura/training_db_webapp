b4964d4a191af0d61003122cfe11b15eeb7b1a1c
Shigeo Kitamura <shigeo.kitamura@supership.jp>
2019-05-14 15:51:59 +0900

    create delete page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,4 +5,13 @@ class CoursesController < ApplicationController
   def search
     @courses = Course.all
   end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def delete
+    @course = Course.find(params[:id])
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'courses#top'
   get '/search', to: 'courses#search'
+  get '/show/:id', to: 'courses#show', as: 'courses_show'
+  get '/delete/:id', to: 'courses#delete', as: 'courses_delete'
 end
