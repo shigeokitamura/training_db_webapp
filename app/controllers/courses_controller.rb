c29840516e419032ae7b7ee3e80f7848fde7b293
Shigeo Kitamura <shigeo.kitamura@supership.jp>
2019-05-14 18:42:49 +0900

    model create/update/delete

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -10,8 +10,39 @@ class CoursesController < ApplicationController
     @course = Course.find(params[:id])
   end
 
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to course_show_path(id: course.course_id)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    if @course.update_attributes(course_params)
+      render 'show'
+    else
+      render 'show'
+    end
+  end
+
   def delete
     @course = Course.find(params[:id])
   end
+
+  def destroy
+    Course.find(params[:id]).destroy
+    render 'deleted'
+  end
   
+  private
+    def course_params
+      params.require(:course).permit(:course_id, :course_title, :topic, :day_length, :price, :level_id, :category)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'courses#top'
   get '/search', to: 'courses#search'
-  get '/show/:id', to: 'courses#show', as: 'courses_show'
-  get '/delete/:id', to: 'courses#delete', as: 'courses_delete'
+  get '/show/:id', to: 'courses#show', as: 'course_show'
+  get '/new', to: 'courses#new'
+  get '/delete/:id', to: 'courses#delete', as: 'course_delete'
+  get '/deleted', to: 'courses#deleted'
+
+  resources :courses
 end
