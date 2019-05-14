96988500743797577e96b78af3b88a79d5fc3b78
Shigeo Kitamura <shigeo.kitamura@supership.jp>
2019-05-14 14:00:27 +0900

    create top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'courses#top'
   get 'courses/top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
