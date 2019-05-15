ecb51b2d07116f51fd61db681706b44703d1b4d6
Shigeo Kitamura <shigeo.kitamura@supership.jp>
2019-05-15 14:43:13 +0900

    new validation

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -17,7 +17,7 @@ class CoursesController < ApplicationController
   def create
     @course = Course.new(course_params)
     if @course.save
-      redirect_to course_show_path(id: course.course_id)
+      redirect_to course_show_path(id: @course.course_id)
     else
       render 'new'
     end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,45 @@
 class Course < ApplicationRecord
   self.table_name = "course"
+
+  validates :course_id,
+    presence: true,
+    length: { maximum: 20 },
+    format: { with: /\A[0-9A-Za-z-\--]+\z/ },
+    uniqueness: true
+
+  validates :course_title,
+    presence: true,
+    length: { maximum: 50 }
+
+  validates :topic,
+    length: { maximum: 100 }
+    
+  validates :day_length,
+    presence: true,
+    numericality: { 
+      only_integer: true,
+      greater_than_or_equal_to: 1,
+      less_than_or_equal_to: 7,
+    }
+
+  validates :price,
+    presence: true,
+    numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 0,
+      less_than_or_equal_to: 999999
+    }
+  
+  validates :level_id,
+    presence: true,
+    numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 1,
+      less_than_or_equal_to: 5
+    }
+  
+  validates :category,
+    presence: true,
+    length: { maximum: 40 },
+    format: { with: /\A[0-9A-Za-z]+\z/ }
 end
