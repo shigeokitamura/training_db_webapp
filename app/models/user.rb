f90b3d5b0164e53d3241a07fdf4ccf641e3fae86
Shigeo Kitamura <shigeo.kitamura@supership.jp>
2019-05-17 11:56:52 +0900

    user validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,27 @@
 class User < ApplicationRecord
   self.table_name = "users"
+
+  validates :user_id,
+            presence: true,
+            length: { maximum: 20 },
+            uniqueness: true
+
+  validates :user_name,
+            presence: true,
+            length: { maximum: 50 }
+
+  validates :age,
+            presence: true,
+            length: { maximum: 2 }
+  
+  validates :dept,
+            presence: true,
+            length: { maximum: 2 }
+  
+  has_secure_password
+  validates :password,
+            presence: true,
+            length: { minimum: 6 }
 end
 
 # rails g model User --migration false
