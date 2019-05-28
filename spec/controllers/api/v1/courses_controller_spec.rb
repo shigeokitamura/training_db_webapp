1da3924bf48cc2188b86de69d8b7a16725cb0556
Shigeo Kitamura <shigeo.kitamura@supership.jp>
2019-05-28 18:32:33 +0900

    api rspec

--- spec/controllers/api/v1/courses_controller_spec.rb
+++ spec/controllers/api/v1/courses_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::CoursesController, type: :controller do
+module Api
+  module V1
+    RSpec.describe CoursesController, type: :controller do
+      describe 'GET #index' do
+        before { FactoryBot.create(:course) }
+        before { get :index }
+        it 'has a 200 status code' do
+          expect(response).to have_http_status(:ok)
+        end
+      end
 
+      describe 'GET #index with parameters' do
+        before { FactoryBot.create(:course) }
+        params = { keyword: "hoge" }
+        before { get :index, params: { get: params } }
+        it 'has a 200 status code' do
+          expect(response).to have_http_status(:ok)
+        end
+        it 'has correct json data' do
+          json = JSON.parse(response.body)
+          expect(json['data'].length).to eq(1)
+        end
+      end
+
+    end
+  end
 end
